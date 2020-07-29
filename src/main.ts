8ed226eca969b88009848d66b3d0fdabe369f43c
stepansavka <stepansavka@protonmail.com>
2020-07-29 07:46:44 +0700

    dashboard-angular

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+/*!
+
+=========================================================
+* Dashboard Angular - v1.2.0
+=========================================================
+
+
+* Coded by Sorin. P
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import 'hammerjs';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
